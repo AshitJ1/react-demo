1bb48d4ee6c8aa6acfbeaa0af7a5f6068aa6ae0c
AshitClover <Ashit.Jhingan@cloverbaytechnologies.com>
2023-02-07 17:38:14 +0530

    urls improvements

--- src/screens/Authenticated/Crud.js
+++ src/screens/Authenticated/Crud.js
@@ -15,6 +15,11 @@ export default function Crud() {
         setUsers(result.data.reverse());
     };
 
+    const deleteUser = async id => {
+        await axios.delete(URLS.deleteUserById(id));
+        loadUsers();
+      };
+
     return (
         <div>
             <Navbar />
@@ -22,31 +27,34 @@ export default function Crud() {
                 <div className="py-4">
                     <h3>Users</h3>
                     {users?.length > 0 ? (
-                    <div className="table-responsive">
-                        <table className="table border shadow">
-                            <thead className="table-dark">
-                                <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">Name</th>
-                                    <th scope="col">Address</th>
-                                    <th scope="col">Email</th>
-                                    <th>Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {users.map((user, index) => (
+                        <div className="table-responsive">
+                            <table className="table border shadow">
+                                <thead className="table-dark">
                                     <tr>
-                                        <th scope="row">{index + 1}</th>
-                                        <td>{user.name}</td>
-                                        <td>{user.address.city}</td>
-                                        <td>{user.email}</td>
-
+                                        <th scope="col">#</th>
+                                        <th scope="col">Name</th>
+                                        <th scope="col">Address</th>
+                                        <th scope="col">Email</th>
+                                        <th>Action</th>
                                     </tr>
-                                ))}
-                            </tbody>
-                        </table>
-                    </div>
-                    ):(<h1>no user found</h1>)}
+                                </thead>
+                                <tbody>
+                                    {users.map((user, index) => (
+                                        <tr>
+                                            <th scope="row">{index + 1}</th>
+                                            <td>{user.name}</td>
+                                            <td>{user.address.city}</td>
+                                            <td>{user.email}</td>
+                                            <td>
+                                                <button type="button" class="btn btn-primary m-2"><i class="fa fa-eye"></i></button>
+                                                <button type="button" class="btn btn-warning m-2"><i class='fas fa-edit'></i></button>
+                                                <button type="button" class="btn btn-danger m-2"  onClick={() => deleteUser(user.id)}><i class="fa fa-trash"></i></button>                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
+                    ) : (<h1>no user found</h1>)}
                 </div>
             </div>
         </div>
--- src/utils/constants/urls.js
+++ src/utils/constants/urls.js
@@ -1,4 +1,4 @@
 export const URLS = {
     users:"http://localhost:3003/users",
-    
+    deleteUserById: (id) => `http://localhost:3003/users/${id}`,
 };
