bf2d3809712c88e0a8fb61e0c029fe4718314d23
AshitClover <Ashit.Jhingan@cloverbaytechnologies.com>
2023-02-28 00:18:17 +0530

    started crud

---
+++ src/screens/Authenticated/Crud/Crud.tsx
@@ -0,0 +1,17 @@
+import React, { FC, useCallback, useEffect, useState } from "react";
+import{
+    AppBar,
+    Box,
+    Toolbar,
+    IconButton,
+    Typography,
+    Menu,
+    Container,
+    Avatar,
+    Button,
+    Tooltip,
+    MenuItem
+}  from "@mui/material";
+import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
+
+
