9c4b214916d5d3c6b7069a21adece8da0807a1a1
AshitJ1 <Ashit.Jhingan@gmail.com>
2024-11-02 16:03:35 +0530

    added QR package

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,26 +1,30 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes as Switch } from 'react-router-dom';
-import { routes } from '../utils/constants/routes'
-import Crud from '../screens/Authenticated/Crud';
-import Utilities from '../screens/Authenticated/Utilities';
-import Editor from '../screens/Authenticated/Editor';
-import Mode from '../screens/Authenticated/Mode';
-import Error from '../common/Error';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes as Switch,
+  Navigate,
+} from "react-router-dom";
+import { routes } from "../utils/constants/routes";
+import Crud from "../screens/Authenticated/Crud";
+import Utilities from "../screens/Authenticated/Utilities";
+import Editor from "../screens/Authenticated/Editor";
+import Mode from "../screens/Authenticated/Mode";
+import Error from "../common/Error";
 
 function Index() {
-
-    return (
-        <Router>
-            <Switch>
-                <Route exact path={routes.Crud} element={<Crud />} />
-                <Route exact path={routes.Utilities} element={<Utilities />} />
-                <Route exact path={routes.Editor} element={<Editor />} />
-                <Route exact path={routes.Mode} element={<Mode />} />
-                <Route path="*" element={<Error />} />
-            </Switch>
-        </Router>
-    )
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" element={<Navigate to={routes.Crud} />} />
+        <Route exact path={routes.Crud} element={<Crud />} />
+        <Route exact path={routes.Utilities} element={<Utilities />} />
+        <Route exact path={routes.Editor} element={<Editor />} />
+        <Route exact path={routes.Mode} element={<Mode />} />
+        <Route path="*" element={<Error />} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default Index;
-
--- src/screens/Authenticated/Utilities.js
+++ src/screens/Authenticated/Utilities.js
@@ -1,13 +1,71 @@
-import React from 'react'
-import Navbar from '../../routes/AuthenticatedLayout/Navbar'
+import React, { useState, useMemo } from "react";
+import Navbar from "../../routes/AuthenticatedLayout/Navbar";
+import QRCode from "react-qr-code";
 
 function Utilities() {
+  // example of useMemo
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([]);
+
+  const increment = () => {
+    setCount((c) => c + 1);
+  };
+  const addTodo = () => {
+    setTodos((t) => [...t, "New Todo"]);
+  };
+
+  const expensiveCalculation = (num) => {
+    console.log("Calculating...");
+    for (let i = 0; i < 1000000000; i++) {
+      num += 1;
+    }
+    return num;
+  };
+
+  // const calculation = expensiveCalculation(count);
+  const calculation = useMemo(() => expensiveCalculation(count), [count]);
+
+  // useMemo memorize the function & it notes into the cache & will rerender only when the variable/dependancy changes
+
   return (
     <div>
       <Navbar />
       <p>utils</p>
+      <div>
+        <div>
+          <h2>My Todos</h2>
+          {todos.map((todo, index) => {
+            return <p key={index}>{todo}</p>;
+          })}
+          <button onClick={addTodo}>Add Todo</button>
+        </div>
+        <hr />
+        <div>
+          Count: {count}
+          <button onClick={increment}>+</button>
+          <h2>Expensive Calculation</h2>
+          {calculation}
+        </div>
+      </div>
+
+      <div
+        style={{
+          height: "auto",
+          margin: "0 auto",
+          maxWidth: 64,
+          width: "100%",
+        }}
+      >
+        <QRCode
+          size={256}
+          style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+          // value={value}
+          value="hi"
+          viewBox={`0 0 256 256`}
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Utilities
+export default Utilities;
